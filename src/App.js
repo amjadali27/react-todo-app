35d9b098950ac260a12034dc3e77e05fd3a3983d
Amjad Ali <ali.amjad@systemsltd.com>
2021-10-14 02:03:51 +0500

    Added View Todos functionality and UI of delete TODO

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import {Todos} from './MyComponents/Todos';
 import {Footer} from './MyComponents/Footer';
 
 function App() {
+
+  let todos = [
+    {
+        sno: 1,
+        title: "Go to Gulshan",
+        desc: "I have to go to Gulshan-e-Iqbal market to make new glasses"
+    },
+    {
+        sno: 2,
+        title: "Hair Cutting",
+        desc: "I need to have a hair cut today"
+    },
+    {
+        sno: 3,
+        title: "Packing",
+        desc: "I have to complete packing tonight"
+    }
+]
+
+const onDelete = (todo)=>{
+  console.log("I am onDelete of todo: ",todo);
+}
+
   return (
     <>
-      <Header title ="My Todos List" searchBar={true}/> 
-      <Todos />
+      <Header title ="My Todos List" searchBar={false}/> 
+      <Todos todos = {todos} onDelete={onDelete}/>
       <Footer />
-    </>
+    </> 
   );
 }
 
--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export const TodoItem = () => {
+export const TodoItem = ({todo,onDelete}) => {
     return (
         <div>
-            TodoItem works!
+           <h5>{todo.title}</h5>
+           <p>{todo.desc}</p>
+           <button className="btn btn-sm btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button>
         </div>
     )
 }
