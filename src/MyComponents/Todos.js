d36650ac26301bf6f640ed01eac099b93e2e3cef
Amjad Ali <ali.amjad@systemsltd.com>
2021-10-15 14:52:51 +0500

    Changes in UI

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -4,7 +4,7 @@ import { TodoItem } from './TodoItem';
 export const Todos = (props) => {
     return (
         <div className="container">
-            <h3 className="text-center my-3">Todos List</h3>
+            <h3 className="text-center my-3"> My Todos List</h3>
             {
                 props.todos.map((todo)=>{
                     return <TodoItem todo={todo} key= {todo.sno} onDelete={props.onDelete}></TodoItem>
